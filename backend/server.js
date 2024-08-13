f91300660ef3bb1332d741d044baf5711a0a9e90
Yasiru Kaveeshwara <152960674+YasiruKaveeshwara@users.noreply.github.com>
2024-08-13 23:46:43 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3050; //Do not change port if you want to change please let other know
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB connection success!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// this is only sample code
+/*
+const travelAgencyRouter = require("./routes/travelAgency"); // import every models
+app.use("/travelAgencyList", travelAgencyRouter); // url for page
+*/
